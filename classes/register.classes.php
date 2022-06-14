e62a2dbf7aee0a3b5c19cfea556f85568ca77219
Nikola Perisic <5dzoni5@gmail.com>
2022-06-14 22:07:38 +0100

    Finished Registration

--- classes/register.classes.php
+++ classes/register.classes.php
@@ -9,28 +9,22 @@ class Register extends Dbh {
 
 
     protected function setUser($uid, $pwd, $email) {
-        $stmt = $this->connect()->prepare('INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ?, ?;)');
-
+        $stmt = $this->connect()->prepare('INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ?, ?)');
         $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-
         if ($stmt->execute(array($uid, $hashedPwd, $email))) {
-
             $stmt = null;
-
-            header("location: ../index.php?error=stmt1failed");
-
+            header("location: ../index.php");
             exit();
-            
         }
 
-        $stmt = null;
+
         
-        // if ($stmt->rowCount() > 0) {
-        //     $resultCheck = false;
-        // } else {
-        //     $resultCheck = true;
-        // }
-        // return $resultCheck;
+        if ($stmt->rowCount() > 0) {
+            $resultCheck = false;
+        } else {
+            $resultCheck = true;
+        }
+        return $resultCheck;
     }
 
 
